8d1483cbfa17c58c57d3b3dd053607d41e9e7e55
JediNakDev <95126886+JediNakDev@users.noreply.github.com>
2024-01-04 16:37:21 +0700

    Make it responsive

--- app/page.tsx
+++ app/page.tsx
@@ -29,13 +29,13 @@ export default function Home() {
   return (
     <div className="w-screen h-screen bg-slate-900 flex justify-center items-center">
       <div className="w-4/5">
-        <div className="text-indigo-500 text-6xl text-justify quote-font">
+        <div className="text-indigo-500 text-xl sm:text-2xl md:text-4xl lg:text-6xl text-justify quote-font">
           <h1 className={quote_font.className}>&#34; {quote.quote} &#34;</h1>
         </div>
-        <div className="text-indigo-700 text-4xl text-right mt-4 character-font">
+        <div className="text-indigo-700 text-based sm:text-xl md:text-2xl lg:text-4xl text-right mt-1 sm:mt-2 md:mt-3 lg:mt-4 character-font">
           <h2 className={character_font.className}>-{quote.character}-</h2>
         </div>
-        <div className="text-indigo-800 text-3xl text-right mr-5 anime-font">
+        <div className="text-indigo-800 text-sm  sm:text-based md:text-lg lg:text-3xl text-right mr-2 md:mr-3 lg:mr-5 anime-font">
           <h3 className={anime_font.className}>{quote.anime}</h3>
         </div>
       </div>
